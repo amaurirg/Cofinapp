bf1aa178e157da01655aceeb50fdbad230966c12
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2018-04-04 01:33:47 -0300

    Estruturando DB

--- cofinapp/core/admin.py
+++ cofinapp/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Aplicativos, Combustivel, Ganhos_Diarios
 
-# Register your models here.
+
+admin.site.register(Aplicativos)
+admin.site.register(Combustivel)
+admin.site.register(Ganhos_Diarios)
